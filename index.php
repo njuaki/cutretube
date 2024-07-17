d577aa195505130cd5268d37f3a4f1a3bd9c7458
Pedro Joaquin Garcia <juaki@juaki.com>
2024-07-17 11:48:05 +0200

    Centrado buscador de inicio

--- index.php
+++ index.php
@@ -16,6 +16,23 @@
             flex: 1;
             padding-bottom: 50px; /* Espacio para el pie de página */
         }
+        .centered-container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 80vh; /* Altura del viewport para centrar verticalmente */
+        }
+
+        .top-container {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .hidden {
+            display: none;
+        }
+
         .modal {
             display: none;
             position: fixed;
@@ -29,6 +46,7 @@
             justify-content: center;
             align-items: center;
         }
+
         .modal-content {
             background-color: #fefefe;
             padding: 20px;
@@ -38,6 +56,7 @@
             text-align: center;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
         }
+
         .loader {
             border: 16px solid #f3f3f3;
             border-radius: 50%;
@@ -47,10 +66,46 @@
             animation: spin 2s linear infinite;
             margin: 0 auto;
         }
+
         @keyframes spin {
             0% { transform: rotate(0deg); }
             100% { transform: rotate(360deg); }
         }
+
+        form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            max-width: 600px;
+            width: 100%;
+            margin: 0 auto 40px;
+        }
+
+        form input[type="text"] {
+            width: 80%;
+            padding: 10px;
+            font-size: 16px;
+            border: 2px solid #ccc;
+            border-radius: 4px 0 0 4px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+form button {
+    padding: 10px 20px;
+    font-size: 16px;
+    border: none;
+    background-color: #FF0000;
+    color: #fff;
+    border-radius: 0 4px 4px 0;
+    cursor: pointer;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    margin-left: 10px; /* Añade un poco de separación */
+}
+
+        form button:hover {
+            background-color: #cc0000;
+        }
+
         footer {
             text-align: center;
             padding: 10px 0;
@@ -61,6 +116,7 @@
             position: fixed;
             bottom: 0;
         }
+        
         .github-link {
             display: inline-flex;
             align-items: center;
@@ -68,6 +124,7 @@
             color: inherit;
             margin-left: 10px;
         }
+        
         .github-link svg {
             width: 20px;
             height: 20px;
@@ -77,129 +134,134 @@
 </head>
 <body>
     <div class="content">
-        <h1><span class="cutre">CUTRE</span><span class="tube">TUBE</span></h1>
-        <h2>Ahora un poco menos cutre</h2>
-        <form method="GET" action="index.php">
-            <input type="text" name="query" placeholder="Introduce tu búsqueda" required>
-            <button type="submit">Buscar</button>
-        </form>
-
-        <div id="modal" class="modal">
-            <div class="modal-content">
-                <h2>Haciendo cosas...</h2>
-                <div class="loader"></div>
-                <p>No tardará mucho, o sí...</p>
-            </div>
+        <div id="header" class="centered-container">
+            <h1><span class="cutre">CUTRE</span><span class="tube">TUBE</span></h1>
+            <h2>Ahora un poco menos cutre</h2>
+            <form id="search-form" method="GET" action="index.php">
+                <input type="text" name="query" placeholder="Introduce tu búsqueda" required>
+                <button type="submit">Buscar</button>
+            </form>
         </div>
 
-        <?php
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
+        <div id="results" class="hidden">
+            <div id="modal" class="modal">
+                <div class="modal-content">
+                    <h2>Haciendo cosas...</h2>
+                    <div class="loader"></div>
+                    <p>No tardará mucho, o sí...</p>
+                </div>
+            </div>
 
-        function vaciarCarpeta($carpeta) {
-            $ficheros = glob($carpeta . '/*');
-            foreach ($ficheros as $fichero) {
-                if (is_file($fichero)) {
-                    unlink($fichero);
+            <?php
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
+
+            function vaciarCarpeta($carpeta) {
+                $ficheros = glob($carpeta . '/*');
+                foreach ($ficheros as $fichero) {
+                    if (is_file($fichero)) {
+                        unlink($fichero);
+                    }
                 }
             }
-        }
 
-        function descargarImagen($url, $rutaDestino) {
-            $img = file_get_contents($url);
-            file_put_contents($rutaDestino, $img);
-        }
+            function descargarImagen($url, $rutaDestino) {
+                $img = file_get_contents($url);
+                file_put_contents($rutaDestino, $img);
+            }
 
-        function mostrarResultados($videos) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-            if (count($videos) > 0) {
-                echo '<h2>Resultados de la búsqueda:</h2>';
-                echo '<div class="grid-container">';
+            function mostrarResultados($videos) {
+                $ip = $_SERVER['REMOTE_ADDR'];
+                if (count($videos) > 0) {
+                    echo '<h2>Resultados de la búsqueda:</h2>';
+                    echo '<div class="grid-container">';
+                    foreach ($videos as $video) {
+                        if (isset($video['videoRenderer'])) {
+                            $title = htmlspecialchars($video['videoRenderer']['title']['runs'][0]['text']);
+                            $thumbnailUrl = htmlspecialchars($video['videoRenderer']['thumbnail']['thumbnails'][0]['url']);
+                            $videoId = $video['videoRenderer']['videoId'];
+                            $downloadLink = "download.php?videoId=$videoId&ip=$ip";
+
+                            $thumbnailPath = "tmpIma/$videoId.jpg";
+                            if (!empty($thumbnailUrl)) {
+                                descargarImagen($thumbnailUrl, $thumbnailPath);
+                            } else {
+                                echo "<p>Error: No se pudo obtener la URL de la miniatura para el video $videoId.</p>";
+                            }
+
+                            echo '<div class="grid-item">';
+                            echo "<a href=\"$downloadLink\" class=\"download-link\" data-video-id=\"$videoId\" data-ip=\"$ip\"><img src=\"$thumbnailPath\" alt=\"Thumbnail\"></a>";
+                            echo "<div class='grid-item-title'>$title</div>";
+                            echo "<a class='ver-video download-link' href=\"$downloadLink\" data-video-id=\"$videoId\" data-ip=\"$ip\">Ver video</a>";
+                            echo '</div>';
+                        }
+                    }
+                    echo '</div>';
+                } else {
+                    echo '<p>No se encontraron resultados. Verifica si la estructura JSON de YouTube ha cambiado.</p>';
+                }
+            }
+
+            function guardarRecomendaciones($videos) {
+                $recommendations = array();
+                $count = 0;
+
                 foreach ($videos as $video) {
-                    if (isset($video['videoRenderer'])) {
+                    if (isset($video['videoRenderer']) && $count < 5) {
                         $title = htmlspecialchars($video['videoRenderer']['title']['runs'][0]['text']);
                         $thumbnailUrl = htmlspecialchars($video['videoRenderer']['thumbnail']['thumbnails'][0]['url']);
                         $videoId = $video['videoRenderer']['videoId'];
-                        $downloadLink = "download.php?videoId=$videoId&ip=$ip";
 
-                        $thumbnailPath = "tmpIma/$videoId.jpg";
-                        if (!empty($thumbnailUrl)) {
-                            descargarImagen($thumbnailUrl, $thumbnailPath);
-                        } else {
-                            echo "<p>Error: No se pudo obtener la URL de la miniatura para el video $videoId.</p>";
-                        }
+                        $recommendations[] = array(
+                            'title' => $title,
+                            'thumbnail' => $thumbnailUrl,
+                            'videoId' => $videoId
+                        );
 
-                        echo '<div class="grid-item">';
-                        echo "<a href=\"$downloadLink\" class=\"download-link\" data-video-id=\"$videoId\" data-ip=\"$ip\"><img src=\"$thumbnailPath\" alt=\"Thumbnail\"></a>";
-                        echo "<div class='grid-item-title'>$title</div>";
-                        echo "<a class='ver-video download-link' href=\"$downloadLink\" data-video-id=\"$videoId\" data-ip=\"$ip\">Ver video</a>";
-                        echo '</div>';
+                        $count++;
                     }
                 }
-                echo '</div>';
-            } else {
-                echo '<p>No se encontraron resultados. Verifica si la estructura JSON de YouTube ha cambiado.</p>';
+
+                file_put_contents('recommendations.json', json_encode($recommendations));
             }
-        }
 
-        function guardarRecomendaciones($videos) {
-            $recommendations = array();
-            $count = 0;
+            vaciarCarpeta('tmpIma');
 
-            foreach ($videos as $video) {
-                if (isset($video['videoRenderer']) && $count < 5) {
-                    $title = htmlspecialchars($video['videoRenderer']['title']['runs'][0]['text']);
-                    $thumbnailUrl = htmlspecialchars($video['videoRenderer']['thumbnail']['thumbnails'][0]['url']);
-                    $videoId = $video['videoRenderer']['videoId'];
+            if (isset($_GET['query'])) {
+                echo "<script>document.getElementById('header').classList.remove('centered-container');document.getElementById('header').classList.add('top-container');document.getElementById('results').classList.remove('hidden');</script>";
+                $query = urlencode($_GET['query']);
+                $url = "https://www.youtube.com/results?search_query=$query";
 
-                    $recommendations[] = array(
-                        'title' => $title,
-                        'thumbnail' => $thumbnailUrl,
-                        'videoId' => $videoId
-                    );
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/58.0.3029.110 Safari/537.3');
+                $response = curl_exec($ch);
 
-                    $count++;
+                if (curl_errno($ch)) {
+                    echo '<p>Error fetching the page: ' . curl_error($ch) . '</p>';
                 }
-            }
 
-            file_put_contents('recommendations.json', json_encode($recommendations));
-        }
+                curl_close($ch);
 
-        vaciarCarpeta('tmpIma');
+                if ($response !== false) {
+                    if (preg_match('/ytInitialData = (.*?);<\/script>/', $response, $matches)) {
+                        $jsonData = json_decode($matches[1], true);
 
-        if (isset($_GET['query'])) {
-            $query = urlencode($_GET['query']);
-            $url = "https://www.youtube.com/results?search_query=$query";
+                        $videos = $jsonData['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'];
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/58.0.3029.110 Safari/537.3');
-            $response = curl_exec($ch);
+                        guardarRecomendaciones($videos);
 
-            if (curl_errno($ch)) {
-                echo '<p>Error fetching the page: ' . curl_error($ch) . '</p>';
-            }
-
-            curl_close($ch);
-
-            if ($response !== false) {
-                if (preg_match('/ytInitialData = (.*?);<\/script>/', $response, $matches)) {
-                    $jsonData = json_decode($matches[1], true);
-
-                    $videos = $jsonData['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'];
-
-                    guardarRecomendaciones($videos);
-
-                    mostrarResultados($videos);
+                        mostrarResultados($videos);
+                    } else {
+                        echo '<p>No se pudo encontrar la estructura JSON en la página de resultados.</p>';
+                    }
                 } else {
-                    echo '<p>No se pudo encontrar la estructura JSON en la página de resultados.</p>';
+                    echo '<p>Error al obtener la página de resultados.</p>';
                 }
-            } else {
-                echo '<p>Error al obtener la página de resultados.</p>';
             }
-        }
-        ?>
+            ?>
+        </div>
     </div>
 
     <footer>
